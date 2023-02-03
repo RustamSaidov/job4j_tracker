eb3bdab0328d72fd9a31b8869a1b0bb9bed79a81
Qafas <rustam_ussr@mail.ru>
2023-02-04 01:16:50 +0400

    1. Что такое Mock-объект. [#504866 #387502]

--- src/main/java/ru/job4j/tracker/action/ReplaceAction.java
+++ src/main/java/ru/job4j/tracker/action/ReplaceAction.java
@@ -5,32 +5,6 @@ import ru.job4j.tracker.Store;
 import ru.job4j.tracker.input.Input;
 import ru.job4j.tracker.output.Output;
 
-//public class ReplaceAction implements UserAction {
-//
-//    private final Output out;
-//
-//    public ReplaceAction(Output out) {
-//        this.out = out;
-//    }
-//
-//    @Override
-//    public String name() {
-//        return "=== Edit item ====";
-//    }
-//
-//    @Override
-//    public boolean execute(Input input, Store tracker) {
-//        int id = Integer.parseInt(input.askStr("Enter id: "));
-//        String name = input.askStr("Enter name: ");
-//        if (tracker.replace(id, new Item(name))) {
-//            out.println("Item is successfully replaced!");
-//        } else {
-//            out.println("Wrong id!");
-//        }
-//        return true;
-//    }
-//}
-
 public class ReplaceAction implements UserAction {
 
     private final Output output;
